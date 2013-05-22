ad978607e54c6e0c40970c8aecdc86657157d167
shagun <shaguniitb@gmail.com>
2013-05-22 11:45:06 -0400

    Added code to check for julia returning C objects and using C objects

--- class_lib/myclass.cpp
+++ class_lib/myclass.cpp
@@ -8,8 +8,23 @@ void MyClass::foo()
 
 int main(){
     MyClass a;
+    a.id = 1;
     a.bar = 9;
     a.foo();
-    return a.bar;
+    return a.id;
 }
 
+int test(){
+    return 10;
+}
+    
+MyClass * set_class(int class_id){
+    MyClass *a;
+    a->id = class_id;
+    return a;
+}
+
+int assign_bar(MyClass * m){
+    m->bar = 5;
+    return 0;
+}
--- class_lib/myclass.h
+++ class_lib/myclass.h
@@ -4,4 +4,11 @@ class MyClass
 public:
     void foo();
     int bar;
+    int id;
 };
+extern "C"{
+    int test();
+    int main();
+    MyClass * set_class(int class_id);
+    int assign_bar(MyClass * m);
+}
